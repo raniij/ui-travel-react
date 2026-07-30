073f4440ff8081aa112305dd923046d742a62e23
raniij <36057407+raniij@users.noreply.github.com>
2026-07-30 15:39:22 +0900

    feat: 메인 배너 Swiper 적용

--- src/pages/Main/components/MainBanner.jsx
+++ src/pages/Main/components/MainBanner.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
+import { Navigation, Pagination } from 'swiper/modules'
 
 import banner01 from '../../../assets/images/main/main_banner01.png'
 import banner02 from '../../../assets/images/main/main_banner02.png'
 import banner03 from '../../../assets/images/main/main_banner03.png'
-import banner04 from '../../../assets/images/main/main_banner04.png'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 import './MainBanner.scss'
 
 const mainBanners = [
@@ -23,11 +23,7 @@ const mainBanners = [
   {
     image: banner03,
     alt: '힐링이 필요한 순간, 바다로 떠나요 푸른 바다와 함께하는 완벽한 휴식'
-  },
-  {
-    image: banner04,
-    alt: '야경이 아름다운 도시 BEST! 낮보다 빛나는 도시의 밤을 만나보세요'
-  },
+  }
 ]
 
 const MainBanner = () => {
@@ -37,14 +33,26 @@ const MainBanner = () => {
       
       <div className="banner">
         <Swiper
-          modules={[Navigation]}
+          modules={[Navigation, Pagination]}
           navigation={{
             prevEl: '.btnPrev',
             nextEl: '.btnNext'
           }}
-          slidesPerView={2}
-          spaceBetween={28}
+          pagination={{
+            clickable: true
+          }}
+          slidesPerView={1}
+          spaceBetween={20}
           loop
+          breakpoints={{
+            590: {
+              slidesPerView: 'auto'
+            },
+            1025: {
+              slidesPerView: 2,
+              spaceBetween: 28
+            }
+          }}
         >
           { mainBanners.map(({ image, alt }, index) => (
             <SwiperSlide key={index}>
